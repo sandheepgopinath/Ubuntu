ec8f2d189524c46a9a532557aa7e48826f22e495
sandheep <sandheepgopinath@gmail.com>
2022-01-13 20:34:10 +0530

    NBConvert and Latex installation

---
+++ latex-nbconvert/install.sh
@@ -0,0 +1,8 @@
+#! bin/bash
+
+pip install nbconvert
+
+sudo apt-get install pandoc
+
+sudo apt-get install texlive-xetex texlive-fonts-recommended texlive-plain-generic
+
---
+++ latex-nbconvert/readme.md
@@ -0,0 +1,1 @@
+<h2> Scripts to install latex and NBconvert </h2>
